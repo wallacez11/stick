5dd6c0500bf6c8b26fa78565e5df07901f45f065
wallacez11 <wallace.santos.mail@gmail.com>
2024-02-18 19:11:23 -0300

    remove key

--- src/main.js
+++ src/main.js
@@ -2,13 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
-import { VueRecaptcha } from 'vue-recaptcha'
 import mitt from 'mitt';
 
 const emitter = mitt()
 const app = createApp(App)
 app.config.globalProperties.emitter = emitter
-app.use(VueRecaptcha, {
-    sitekey: '6LfVK3cpAAAAAIciRS8j6ea-3mgTG1aOAGBmq-XB',
-})
+
 app.mount('#app')
